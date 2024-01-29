86b291eeead9f95589e88c60df4a968bb1de3787
ashlyn-mcc <am4915a@american.edu>
2024-01-29 15:29:04 -0500

    added some animation

--- assignments/01_abstract/sketch.js
+++ assignments/01_abstract/sketch.js
@@ -4,34 +4,45 @@
 
 // Create array to hold objects
 let face = [];
-
+let backgroundArray = [];
 
 function setup() {
 	createCanvas(600, 600);
 	background(100);
 	angleMode(DEGREES);
 	colorMode(HSB, 360, 100, 100, 100);
-	frameRate(20);
+	//frameRate(5);
+
+	// function is responsible for generating all of the dot objects
+	dotProduction();
+
 }
 
 function draw() {
 
+	if (mouseY > height){
+		mouseY = height;
+	}
 	// set background
 	background(0);
-	fill(180,70,80);
+	fill(180,70,80 - (mouseY/9));
 	rect(0,0,width,height);
 	
-	// function is responsible for generating all of the dot objects
-	dotProduction();
+	backgroundGenerator(mouseY);
+
+	for (let i = 0; i <backgroundArray.length; i++){
+		backgroundArray[i].display();
+	}
 
 	// go through the array of objects and display each one
 	for (let i = 0; i < face.length; i++) {
 		face[i].display();
+		face[i].randomize();
 	}
 
 	// remove all objects from the array, they will be generated at a new random position
 	// with the next draw iteration
-	face.splice(0, face.length); 
+	backgroundArray.splice(0, backgroundArray.length); 
 }
 
 
@@ -39,7 +50,17 @@ function draw() {
 // give parameters for the potential circular area a dot can be spawned in and the color of the dot
 class Feature {
 
-	constructor(x, y, size, maxWidth, maxHeight, hMin, hMax, sMin, sMax, bMin, bMax, percentMarg, alpha = 100) {
+	constructor(x, y, size, maxWidth, maxHeight, hMin, hMax, sMin, sMax, bMin, bMax, percentMarg, alpha = 100, movement = 0) {
+
+		this.maxWidth = maxWidth;
+		this.maxHeight = maxHeight;
+		this.hMin = hMin;
+		this.hMax = hMax;
+		this.sMin = sMin;
+		this.sMax = sMax;
+		this.bMin = bMin;
+		this.bMax = bMax;
+		this.percentMarg = percentMarg;
 
 		// Center coordinates of the face
 		this.centerX = x + 100;
@@ -50,16 +71,22 @@ class Feature {
 
 		// Randomly generated values for circular positioning
 		this.angle = random(0, 360);
-		this.ampX = random(1, maxWidth);
-		this.ampY = random(1, maxHeight);
+		this.ampX = random(1, this.maxWidth);
+		this.ampY = random(1, this.maxHeight);
 
 		// Calculate x and y position with sine and cosine
 		this.x = this.centerX + this.ampX * cos(this.angle);
 		this.y = this.centerY + this.ampY * sin(this.angle);
 
+		this.initalY = this.y;
+
 		// Percentage calculated based on position comapred to total range of x and y 
 		this.percentX = this.ampX / maxWidth;
 		this.percentY = this.ampY / maxHeight;
+
+		this.value = 0;
+
+		this.position = 0;
 
 		// Generate hue value
 		this.hue = random(hMin, hMax)
@@ -76,25 +103,57 @@ class Feature {
 			this.bright = random(bMin, bMax)
 		}
 
+		this.movement = movement
+
 	}
 
 	// displays the dot given the constructor parameters
 	display() {
+		if (this.movement == 1){
+			this.value = mouseY/35
+		}
 		noStroke();
 		fill(this.hue, this.sat, this.bright, this.alpha);
-		ellipse(this.x, this.y, this.size, this.size);
+		ellipse(this.x, this.y + this.value, this.size, this.size);
+
 	}
+
+	randomize(){
+		// Randomly generated values for circular positioning
+		this.angle = random(0, 360);
+		this.ampX = random(1, this.maxWidth);
+		this.ampY = random(1, this.maxHeight);
+
+		// Calculate x and y position with sine and cosine
+		this.x = this.centerX + this.ampX * cos(this.angle);
+		this.y = this.centerY + this.ampY * sin(this.angle);
+
+		// Generate hue value
+		this.hue = random(this.hMin, this.hMax)
+
+		// Determines saturation and brightness based on position of x and y from center of face
+		if (this.percentX > this.percentMarg && this.percentY > this.percentMarg) {
+			this.sat = random(this.sMin + 5, this.sMax + 10);
+			this.bright = random(this.bMin - 10, this.bMax - 10);
+		} else {
+			this.sat = random(this.sMin, this.sMax)
+			this.bright = random(this.bMin, this.bMax)
+		}
+	}
+
 
 }
 
+function backgroundGenerator(mouseValue){
+	// background
+	for (let i = 0; i < 7000; i++){
+		backgroundArray.push(new Feature(200, 200, 20,600,600,170,200,60,80,70 - (mouseValue/9),90- (mouseValue/9)));
+	}
+}
 
 // responsible for generating the 50000 dot objects in the sketch
 function dotProduction(){
 	
-	// background
-	for (let i = 0; i < 7000; i++){
-		face.push(new Feature(200, 200, 20,600,600,170,200,60,80,70,90));
-	}
 	
 	// hair 
 	for (let i = 0; i < 5000; i++){
@@ -104,7 +163,7 @@ function dotProduction(){
 	}
 
 	// shirt
-	for (let i = 0; i < 10000; i++) {
+	for (let i = 0; i < 15000; i++) {
 		face.push(new Feature(200, 600, 20, 275, 275, 325, 340, 40, 50, 75, 95, 0.85));
 	}
 	
@@ -159,14 +218,14 @@ function dotProduction(){
 	
 	// eye lashes
 	for (let i = 0; i < 50; i++) {
-		face.push(new Feature(250, 170, 2, 22, 2, 25, 35, 25, 40, 0, 5, 1));
-		face.push(new Feature(150, 170, 2, 22, 2, 25, 35, 25, 40, 0, 5, 1));
+		face.push(new Feature(250, 170, 2, 22, 2, 25, 35, 25, 40, 0, 5, 1, 100, 1));
+		face.push(new Feature(150, 170, 2, 22, 2, 25, 35, 25, 40, 0, 5, 1, 100, 1));
 	}
 
 	// eye lids
-	for (let i = 0; i < 50; i++) {
-		face.push(new Feature(250, 165, 5, 30, 5, 25, 35, 25, 40, 90, 95, 0.85));
-		face.push(new Feature(150, 165, 5, 30, 5, 25, 35, 25, 40, 90, 95, 0.85));
+	for (let i = 0; i < 250; i++) {
+		face.push(new Feature(250, 160, 5, 30, 10, 25, 35, 25, 40, 90, 95, 0.85, 100, 1));
+		face.push(new Feature(150, 160, 5, 30, 10, 25, 35, 25, 40, 90, 95, 0.85, 100, 1));
 	}
 
 	// eyebrows
@@ -251,3 +310,8 @@ function dotProduction(){
 
 
 
+
+
+// Notes:
+// Alter eyelids, eyebrows, lips
+// Mouse Y position corresponds to movement of features
