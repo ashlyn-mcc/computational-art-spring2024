bd54347538f5b8dd312d193e3a48563ab1f3f6df
ashlyn-mcc <am4915a@american.edu>
2024-02-07 17:01:10 -0500

    lightpost instead of jellyfish
    went different direction with it

--- assignments/02_noise/sketch.js
+++ assignments/02_noise/sketch.js
@@ -1,139 +1,214 @@
-// 02_noise Assignment
-// CSC-496 Computational Art 
-
-let bloom = [];
-
 function setup() {
-	createCanvas(800, 800);
+	createCanvas(600, 700);
+	colorMode(HSB, 360, 100, 100, 100)
 	background(0);
-	colorMode(HSB,360,100,100,100);
-	angleMode(DEGREES);
+	angleMode(DEGREES)
 
 
-	// for (let i = 0; i < 2; i++){
-	// 	bloom.push(new Jellyfish(random(100,width-100),random(100,height-100),random(75,100)));
-	// }
-
-	for (let i = 1500; i > 0; i--){
-		fill(210,i/15,i/60);
-		noStroke();
-		ellipse(400,200,i)
+	for (let i = 0; i < 40; i++) {
+		offset.push(random(0, 100000));
 	}
+
+	for (let i = 0; i < 301; i++) {
+		strokeWeight(1);
+		stroke(0, 0, map(i, 0, 301, 0, 10))
+		line(i, 0, i, height);
+		line(width - i, 0, width - i, height);
+	}
+
+	bkgrdfill()
+	Steps()
+
+	lightfixture();
+
+
+
+
+
 }
 
 function draw() {
-
-	console.log(frameCount);
-
-	fill(0,0,0,1);
-
-	for (let i = 0; i < bloom.length; i++){
-	bloom[i].dome();
-	bloom[i].flowyPart();
-	}
+	flow();
 }
 
-function mouseClicked(){
-	bloom.push(new Jellyfish(mouseX,mouseY,random(45,100)))
+function Steps() {
+
+
+	stroke(0, 0, 0)
+	strokeWeight(2)
+
+	line(95, 600, 15, 700)
+	line(505, 600, 585, 700)
+
+	line(95, 600, 505, 600) //1st step
+	line(95, 575, 505, 575)
+	line(95, 575, 95, 600)
+	line(505, 575, 505, 600)
+
+	line(115, 555, 485, 555)
+	line(95, 575, 115, 555)
+	line(505, 575, 485, 555)
+
+	line(485, 555, 485, 535)
+	line(115, 555, 115, 535)
+	line(485, 535, 115, 535)
+
+	line(130, 520, 470, 520)
+	line(130, 520, 115, 535)
+	line(470, 520, 485, 535)
+
+	line(130, 520, 130, 505)
+	line(470, 520, 470, 505)
+	line(130, 505, 470, 505)
+
+	line(190, 450, 410, 450)
+	line(190, 450, 130, 505)
+	line(470, 505, 410, 450)
+
+	line(410, 450, 410, 438)
+	line(190, 450, 190, 438)
+	line(190, 438, 410, 438)
+
+	line(205, 428, 395, 428)
+	line(190, 438, 205, 428)
+	line(410, 438, 395, 428)
+
+	line(205, 428, 205, 420)
+	line(395, 428, 395, 420)
+	line(205, 420, 395, 420)
+
+	line(215, 412, 385, 412)
+	line(215, 412, 205, 420)
+	line(395, 420, 385, 412)
+
+	line(385, 412, 385, 405)
+	line(215, 412, 215, 405)
+	line(385, 405, 215, 405)
+
+	line(255, 380, 345, 380)
+	line(255, 380, 215, 405)
+	line(345, 380, 385, 405)
+
+	line(255, 380, 255, 375)
+	line(345, 380, 345, 375)
+	line(255, 375, 345, 375)
+
+	line(265, 370, 335, 370)
+	line(265, 370, 255, 375)
+	line(335, 370, 345, 375)
+
+
 }
 
-class Jellyfish{
-	constructor(x,y,size){
-		this.x = x;
-		this.y = y;
-		this.size = size;
-		this.hue = random(0,360);
-		this.spacing = this.size/6.67;
-		this.minAngle = 0;
-		this.maxAngle = 0;
-		
-		this.offset = [];
-		this.xVertices = [];
-		this.yVertices = [];
+function bkgrdfill() {
 
-		this.extend = random(2,4)
-		for (let i = 0; i < 40; i++){
-			this.offset.push(random(0,100000));
-		}
+	fill(220, 0, 6)
+	quad(15, 700, 585, 700, 505, 600, 95, 600) //steps
+	fill(220, 0, 8)
+	quad(95, 575, 505, 575, 485, 555, 115, 555)
+	fill(220, 0, 10)
+	quad(115, 535, 485, 535, 470, 520, 130, 520)
+	fill(220, 0, 12)
+	quad(130, 505, 470, 505, 410, 450, 190, 450)
+	fill(220, 0, 14)
+	quad(190, 438, 410, 438, 397, 428, 203, 428)
+	fill(220, 0, 16)
+	quad(205, 420, 395, 420, 385, 410, 215, 410)
+	fill(220, 0, 18)
+	quad(215, 405, 385, 405, 345, 380, 255, 380)
+	fill(220, 0, 20)
+	quad(255, 375, 345, 375, 335, 370, 265, 370)
 
-		this.amount = random(0,100000);
-		this.rotate = random(15,45);
+	fill(220, 0, 4)
+	rect(95, 575, 410, 25)
+	fill(220, 0, 5)
+	rect(115, 535, 370, 20)
+	fill(220, 0, 6)
+	rect(130, 505, 340, 15)
+	fill(220, 0, 7)
+	rect(190, 438, 220, 12)
+	fill(220, 0, 8)
+	rect(205, 420, 190, 8)
+	fill(220, 0, 9)
+	rect(215, 405, 170, 5)
+	fill(220, 0, 10)
+	rect(255, 375, 90, 5)
+
+}
+
+function lightfixture() {
+
+	fill(0);
+	rect(325, 220, 1, 150);
+	quad(325, 340, 326, 340, 330, 370, 321, 370);
+	rect(320.5, 220, 10, 2);
+	ellipse(325.5, 219, 3, 3);
+	noFill();
+	stroke(0);
+	arc(315, 220, 10, 15, 180, 360);
+	fill(0);
+	noStroke();
+	ellipse(310, 223, 5, 5);
+	arc(310, 228, 15, 10, 180, 360);
+	ellipse(310, 228, 20, 2);
+	fill(50, 100, 100, 40);
+	arc(310, 228, 5, 5, 0, 180);
+}
+
+let offset = [];
+let xVertices = [];
+let yVertices = [];
+
+let hue = 40
+
+let expand = 0;
+let yexpand = 0;
+
+function flow() {
+	for (let i = 0; i < 5; i++) {
+		xVertices[i] = map(noise(offset[i]), 0, 1, 308 - expand, 310 + expand);
+		yVertices[i] = map(noise(offset[i + 20]), 0, 1, 230, 235 + yexpand);
+
 	}
 
-	dome(){
+	strokeWeight(0.25);
+	stroke(map(frameCount % 360, 0, 360, hue - 20, hue + 20), 50, 100, 1);
+	noFill();
 
-		push();
-		translate(this.x,this.y);
-		rotate(this.rotate);
+	beginShape();
 
-		this.hue = map(noise(this.amount),0,1,0,360)
-		// dome shape
-		strokeWeight(this.size/50);
-		stroke(this.hue,75,40);
-		fill(this.hue,50,100);
-		arc(0,0,this.size,this.size*1.25,180,360);
-
-		// bottom edge dots
-		noStroke();
-		for (let i = 0; i < this.size/this.spacing; i++){
-			ellipse((-this.size/2.2) + this.spacing * i,0,this.spacing,this.spacing);
-		}
-		for (let i = 0; i < this.size/this.spacing; i++){
-			if (i < 3){
-				this.minAngle = 0;
-				this.maxAngle = 230;
-			} else if (i == 3){
-				this.minAngle = 310;
-				this.maxAngle = 230;
-			} else {
-				this.minAngle = 310;
-				this.maxAngle = 180;
-			}
-			fill(0,0,100);
-			noStroke();
-			ellipse((-this.size/2.2) + this.spacing * i,0,this.spacing/2.5,this.spacing/2.5);
-			noFill();
-			stroke(this.hue,75,40);
-			arc((-this.size/2.2) + this.spacing * i,0,this.spacing,this.spacing,this.minAngle,this.maxAngle);
-		}
-
-		this.amount += 0.001
-		pop();
+	for (let i = 0; i < 5; i++) {
+		curveVertex(xVertices[i], yVertices[i])
 	}
 
-	flowyPart(){
-	
-		
-		push()
-		translate(this.x,this.y);
-		rotate(this.rotate);
-		for (let i = 0; i < 5; i++){
-			this.xVertices[i] = map(noise(this.offset[i]),0,1,-this.size/3,this.size/3)
-			this.yVertices[i] = map(noise(this.offset[i+20]),0,1,-this.size,this.size * this.extend)
-
-		}
-		
-		strokeWeight(0.25);
-		stroke(map(frameCount%360,0,360,this.hue-20,this.hue+20),100,100,10);
-		noFill();
-		
-		beginShape();
-
-		for (let i = 0; i < 5; i ++){
-			curveVertex(this.xVertices[i],this.yVertices[i])
-		}
-
-		endShape();
+	endShape();
 
 
 
 
-		for (let i=0; i < this.offset.length; i++){
-			this.offset[i] += 0.01
-		}
-
-		pop();
+	for (let i = 0; i < offset.length; i++) {
+		offset[i] += 0.005
 	}
 
-	
+	if (expand < 100) {
+		expand += 0.1;
+	}
+	if (yexpand < 180) {
+		yexpand += 0.1;
+	}
+
+	if (frameCount % 2500 == 0) {
+		for (let i = 0; i < 301; i++) {
+			strokeWeight(1);
+			stroke(0, 0, map(i, 0, 301, 0, 10))
+			line(i, 0, i, height);
+			line(width - i, 0, width - i, height);
+		}
+		bkgrdfill()
+		Steps()
+		lightfixture();
+		expand = 0;
+		yexpand = 0;
+
+	}
 }
